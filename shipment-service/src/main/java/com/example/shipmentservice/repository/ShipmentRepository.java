45d601009b44cf6bc7dbb9b50bb5051f4782a332
Steven Huang <steven.charles.huang@gmail.com>
2019-12-13 13:59:04 -0500

    fixed bugs

--- shipment-service/src/main/java/com/example/shipmentservice/repository/ShipmentRepository.java
+++ shipment-service/src/main/java/com/example/shipmentservice/repository/ShipmentRepository.java
@@ -1,12 +1,14 @@
 package com.example.shipmentservice.repository;
 
 import com.example.shipmentservice.model.Shipment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShipmentRepository extends CrudRepository<Shipment, Long> {
 
+    @Query(value="FROM Shipment s WHERE s.trackingNumber = :trackingNumber")
     public Shipment findByTrackingNumber(Long trackingNumber);
 
 }
